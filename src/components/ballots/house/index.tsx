5801b449896115fd972b7bc95a83ca44b9abeb45
Robin Stocker <robin@nibor.org>
2022-05-05 11:48:51 +1000

    Allow ranking by clicking box

--- src/components/ballots/house/index.tsx
+++ src/components/ballots/house/index.tsx
@@ -2,6 +2,7 @@ import { FunctionalComponent, h } from "preact";
 import style from "./style.css";
 import houseCandidates from "../../../data/house-candidates.json";
 import { stateNames } from "../constants";
+import { StateUpdater, useState } from "preact/hooks";
 
 interface Props {
   state: string;
@@ -15,10 +16,43 @@ type HouseCandidate = {
   partyBallotName: string;
 };
 
-const renderCandidate = (candidate: HouseCandidate) => {
+const updateRanking = (
+  index: number,
+  ranking: number[],
+  setRanking: StateUpdater<number[]>
+) => {
+  ranking = [...ranking];
+  if (ranking[index] !== undefined) {
+    delete ranking[index];
+    setRanking(ranking);
+  } else {
+    const availableNumbers = Array.from({ length: 10 }, (x, i) => i + 1);
+    for (const rank of ranking) {
+      delete availableNumbers[rank - 1];
+    }
+    const rank = availableNumbers.find((n) => n);
+    if (rank !== undefined) {
+      ranking[index] = rank;
+      setRanking(ranking);
+    }
+  }
+};
+
+const renderCandidate = (
+  candidate: HouseCandidate,
+  index: number,
+  ranking: number[],
+  setRanking: StateUpdater<number[]>
+) => {
+  const onClick = () => {
+    updateRanking(index, ranking, setRanking);
+  };
+
   return (
     <div class={style.candidateDiv}>
-      <div class={style.order} />
+      <div class={style.order} onClick={onClick}>
+        {ranking[index]}
+      </div>
       <div class={style.candidateDetails}>
         <p class={style.candidateName}>
           {candidate.surname}, {candidate.ballotGivenName}
@@ -36,18 +70,22 @@ const HouseBallot: FunctionalComponent<Props> = (props: Props) => {
     division
   ] as HouseCandidate[];
 
+  const [ranking, setRanking] = useState<number[]>(Array(candidates.length));
+
   const stateName = stateNames[state] || state;
 
   return (
-    <div class={style.ballot}>
-      <p class={style.state}>{stateName}</p>
-      <p class={style.division}>Electoral Division of {division}</p>
-      <p class={style.how}>
-        Number the boxes from 1 to {candidates.length} in the order of your
-        choice.
-      </p>
+    <div class={style.ballotContainer}>
+      <div class={style.ballot}>
+        <p class={style.state}>{stateName}</p>
+        <p class={style.division}>Electoral Division of {division}</p>
+        <p class={style.how}>
+          Number the boxes from 1 to {candidates.length} in the order of your
+          choice.
+        </p>
 
-      {candidates.map((c, i) => renderCandidate(c))}
+        {candidates.map((c, i) => renderCandidate(c, i, ranking, setRanking))}
+      </div>
     </div>
   );
 };
