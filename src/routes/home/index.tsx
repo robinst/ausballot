0d9f5456ce493ab757f431fe6b3eba0338b864a5
Robin Stocker <robin@nibor.org>
2022-05-06 14:01:32 +1000

    Welcome page

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -5,8 +5,17 @@ import SelectElectorate from "../../components/select-electorate";
 const Home: FunctionalComponent = () => {
   return (
     <div class={style.home}>
+      <div>
+        <h2>Welcome</h2>
+        <p>
+          Are you voting in the 2022 Australian election? Would you like to see
+          your ballot papers before going to the polls? You've come to the right
+          place.
+        </p>
+        <p>Select your electorate below to get started:</p>
+      </div>
       <SelectElectorate />
-      <p>
+      <p className={style.info}>
         Not sure which electorate you're in? Find out on the{" "}
         <a
           href="https://electorate.aec.gov.au/"
