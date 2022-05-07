73ebda83f369c5a7042a1df762a6286a369ddd23
Robin Stocker <robin@nibor.org>
2022-05-07 23:50:55 +1000

    Add AEC links to electorate page

--- src/routes/electorate/index.tsx
+++ src/routes/electorate/index.tsx
@@ -12,6 +12,11 @@ interface Props {
 const Electorate: FunctionalComponent<Props> = (props: Props) => {
   const { state, division } = props;
 
+  const divisionSlug = division.replace(" ", "-").toLowerCase();
+  const profileLink = `https://www.aec.gov.au/profiles/${state.toLowerCase()}/${divisionSlug}.htm`;
+
+  const candidatesLink = `https://www.aec.gov.au/election/candidates.htm?suburb=${division}&division=${division}&state=${state}`;
+
   return (
     <div class={style.electorate}>
       <label htmlFor="select-electorate" className={style.electorateLabel}>
@@ -30,6 +35,22 @@ const Electorate: FunctionalComponent<Props> = (props: Props) => {
       <h2 class={style.header}>Senate ballot</h2>
 
       <SenateBallot key={state} state={state} />
+
+      <div>
+        <h2 class={style.header}>AEC links</h2>
+        <ul>
+          <li>
+            <a href={profileLink} target="_blank" rel="noreferrer">
+              Electorate profile (maps, etc)
+            </a>
+          </li>
+          <li>
+            <a href={candidatesLink} target="_blank" rel="noreferrer">
+              Candidates list
+            </a>
+          </li>
+        </ul>
+      </div>
 
       <div class={style.stretcher} />
     </div>
