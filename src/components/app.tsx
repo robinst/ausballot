eb676b1442448e92f2bd2838226b80170d79106d
Robin Stocker <robin@nibor.org>
2022-05-08 00:04:47 +1000

    Remove Profile parts (from template)

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,7 +4,6 @@ import { Route, Router } from "preact-router";
 import About from "../routes/about";
 import Electorate from "../routes/electorate";
 import Home from "../routes/home";
-import Profile from "../routes/profile";
 import NotFoundPage from "../routes/notfound";
 import Header from "./header";
 
@@ -14,10 +13,8 @@ const App: FunctionalComponent = () => {
       <Header />
       <Router>
         <Route path="/" component={Home} />
-        <Route path="/profile/" component={Profile} user="me" />
-        <Route path="/profile/:user" component={Profile} />
-        <Route path="/electorate/:state/:division" component={Electorate} />
         <Route path="/about/" component={About} />
+        <Route path="/electorate/:state/:division" component={Electorate} />
         <NotFoundPage default />
       </Router>
     </div>
