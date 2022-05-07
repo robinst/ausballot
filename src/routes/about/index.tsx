8a398ba873adae0a1bf1ec2d2be2ce55709c0dbe
Robin Stocker <robin@nibor.org>
2022-05-07 21:30:43 +1000

    Add note about local storage

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -4,7 +4,20 @@ import style from "./style.css";
 const About: FunctionalComponent = () => {
   return (
     <div class={style.about}>
-      <h1>About</h1>
+      <h2>About</h2>
+      <p>TODO Disclaimer</p>
+      <p>
+        Your vote data is not sent anywhere, it is only stored on your device
+        within your browser using{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Web_storage"
+          target="_blank"
+          rel="noreferrer"
+        >
+          local storage
+        </a>
+        .
+      </p>
       <p>Like this site? How about:</p>
       <a href="https://www.buymeacoffee.com/robinst">
         <img width="200" height="56" src="/assets/bmc-button.png" />
